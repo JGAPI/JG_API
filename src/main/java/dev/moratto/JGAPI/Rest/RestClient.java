b159cc1aa61d19c9de51d9fd33a6d949d735da0c
Nathaniel M <nmoratto629@gmail.com>
2022-05-14 12:41:09 -0700

    public rest methods > private.

--- src/main/java/dev/moratto/JGAPI/Rest/RestClient.java
+++ src/main/java/dev/moratto/JGAPI/Rest/RestClient.java
@@ -12,45 +12,45 @@ import dev.moratto.JGAPI.Entities.Members.ServerMemberSummary;
 import dev.moratto.JGAPI.Entities.Webhooks.Webhook;
 
 public class RestClient {
-    public ChatMessage createChannelMessage(long channelId, ChatMessage message) {}
-    public ChatMessage[] getMessages(long channelId) {}
-    public ChatMessage getMessage(long channelId, long messageId) {}
-    public ChatMessage updateMessage(long channelId, long messageId, ChatMessage message) {}
+    private ChatMessage createChannelMessage(long channelId, ChatMessage message) {}
+    private ChatMessage[] getMessages(long channelId) {}
+    private ChatMessage getMessage(long channelId, long messageId) {}
+    private ChatMessage updateMessage(long channelId, long messageId, ChatMessage message) {}
     public boolean deleteMessage(long channelId, long messageId) {}
-    public String updateNickname(long serverId, long userId, String nickname) {}
-    public boolean deleteNickname(long serverId, long userId) {}
-    public ServerMember getMember(long serverId, long userId) {}
-    public boolean kickMember(long serverId, long userId) {}
-    public ServerMemberSummary[] getMembers(long serverId) {}
-    public ServerMemberBan createServerBan(long serverId, long userId, ServerMemberBan serverMemberBan) {}
-    public ServerMemberBan getServerBan(long serverId, long userId) {}
-    public boolean deleteServerBan(long serverId, long userId) {}
-    public ServerMemberBan[] getServerBan(long serverId) {}
-    public ForumThread createForumThread(long channelId, ForumThread forumThread) {}
-    public ListItem createListItem(long channelId, ListItem listItem) {}
-    public ListItemSummary[] getListItems(long channelId) {}
-    public ListItem getListItem(long channelId, long listItemId) {}
-    public ListItem updateListItem(long channelId, long listItemId, ListItem listItem) {}
-    public boolean deleteListitem(long channelId, long listItemId) {}
-    public boolean completeListItem(long channelId, long listItemId) {}
-    public boolean uncompleteListItem(long channelId, long listItemId) {}
-    public Doc createDoc(long channelId, Doc doc) {}
-    public Doc[] getDocs(long channelId) {}
-    public Doc getDoc(long channelId, long docId) {}
-    public Doc updateDoc(long channelId, long docId) {}
-    public boolean deleteDoc(long channelId, long docId) {}
-    public boolean addReactionEmote(long channelId, long contentId, long emoteId) {}
-    public int awardMemberXP(long serverId, long userId) {}
-    public int awardRoleXP(long serverId, long roleId) {}
-    public JSONObject getSocialLinks(long serverId, long userId, String type) {}
-    public boolean addMemberToGroup(long groupId, long userId) {}
-    public boolean removeMemberFromGroup(long groupId, long userId) {}
-    public boolean addRoleToMember(long serverId, long userId, long roleId) {}
-    public boolean removeRoleFromMember(long serverId, long userId, long roleId) {}
-    public int[] getMemberRoles(long serverId, long userId) {}
-    public Webhook createWebhook(long serverId, Webhook webhook) {}
-    public Webhook[] getWebhooks(long serverId) {}
-    public Webhook getWebhook(long serverId, long webhookId) {}
-    public Webhook updateWebhook(long serverId, long webhookId, Webhook webhook) {}
-    public boolean deleteWebhook(long serverId, long webhookId) {}
+    private String updateNickname(long serverId, long userId, String nickname) {}
+    private boolean deleteNickname(long serverId, long userId) {}
+    private ServerMember getMember(long serverId, long userId) {}
+    private boolean kickMember(long serverId, long userId) {}
+    private ServerMemberSummary[] getMembers(long serverId) {}
+    private ServerMemberBan createServerBan(long serverId, long userId, ServerMemberBan serverMemberBan) {}
+    private ServerMemberBan getServerBan(long serverId, long userId) {}
+    private boolean deleteServerBan(long serverId, long userId) {}
+    private ServerMemberBan[] getServerBan(long serverId) {}
+    private ForumThread createForumThread(long channelId, ForumThread forumThread) {}
+    private ListItem createListItem(long channelId, ListItem listItem) {}
+    private ListItemSummary[] getListItems(long channelId) {}
+    private ListItem getListItem(long channelId, long listItemId) {}
+    private ListItem updateListItem(long channelId, long listItemId, ListItem listItem) {}
+    private boolean deleteListitem(long channelId, long listItemId) {}
+    private boolean completeListItem(long channelId, long listItemId) {}
+    private boolean uncompleteListItem(long channelId, long listItemId) {}
+    private Doc createDoc(long channelId, Doc doc) {}
+    private Doc[] getDocs(long channelId) {}
+    private Doc getDoc(long channelId, long docId) {}
+    private Doc updateDoc(long channelId, long docId) {}
+    private boolean deleteDoc(long channelId, long docId) {}
+    private boolean addReactionEmote(long channelId, long contentId, long emoteId) {}
+    private int awardMemberXP(long serverId, long userId) {}
+    private int awardRoleXP(long serverId, long roleId) {}
+    private JSONObject getSocialLinks(long serverId, long userId, String type) {}
+    private boolean addMemberToGroup(long groupId, long userId) {}
+    private boolean removeMemberFromGroup(long groupId, long userId) {}
+    private boolean addRoleToMember(long serverId, long userId, long roleId) {}
+    private boolean removeRoleFromMember(long serverId, long userId, long roleId) {}
+    private int[] getMemberRoles(long serverId, long userId) {}
+    private Webhook createWebhook(long serverId, Webhook webhook) {}
+    private Webhook[] getWebhooks(long serverId) {}
+    private Webhook getWebhook(long serverId, long webhookId) {}
+    private Webhook updateWebhook(long serverId, long webhookId, Webhook webhook) {}
+    private boolean deleteWebhook(long serverId, long webhookId) {}
 }
