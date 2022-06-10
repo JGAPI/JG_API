a44ce30b504dcaef38f80bd7c167bd2eecd869d8
Nathaniel M <nmoratto629@gmail.com>
2022-06-10 14:58:44 -0700

    Add ListenerAdapters to Builder and Client.

--- src/main/java/dev/JGAPI/JG_API/JG_API.java
+++ src/main/java/dev/JGAPI/JG_API/JG_API.java
@@ -6,10 +6,14 @@ import dev.JGAPI.JG_API.Exceptions.InvalidOperationException;
 import dev.JGAPI.JG_API.Rest.RestClient;
 import dev.JGAPI.JG_API.Websocket.WebSocketManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JG_API extends Thread {
     private String parentServerId;
     private String clientToken;
     private User clientUser;
+    private List<ListenerAdapter> listenerAdapters = new ArrayList<ListenerAdapter>();
 
     private WebSocketManager webSocketManager;
     private RestClient restClient;
@@ -18,6 +22,7 @@ public class JG_API extends Thread {
     private JG_API(ClientBuilder clientBuilder) {
         this.parentServerId = clientBuilder.parentServerId;
         this.clientToken = clientBuilder.clientToken;
+        this.listenerAdapters = clientBuilder.listenerAdapters;
     }
 
     public void login() {
@@ -47,9 +52,14 @@ public class JG_API extends Thread {
         return clientUser;
     }
 
+    public List<ListenerAdapter> getListenerAdapters() {
+        return listenerAdapters;
+    }
+
     public static class ClientBuilder {
         private String parentServerId;
         private String clientToken;
+        private List<ListenerAdapter> listenerAdapters = new ArrayList<ListenerAdapter>();
 
         /**
          * Build the Client
@@ -75,6 +85,12 @@ public class JG_API extends Thread {
             return this;
         }
 
+        public ClientBuilder addListenerAdapter(ListenerAdapter listenerAdapter) {
+            this.listenerAdapters.add(listenerAdapter);
+
+            return this;
+        }
+
         /**
          * Validates the Client Object.
          * TODO: Remove requirement for ClientID.
@@ -88,6 +104,9 @@ public class JG_API extends Thread {
                 throw new ClientBuildException("A Client Token was not provided to the Client Builder.");
             if (client.parentServerId == null || client.parentServerId.isBlank())
                 throw new ClientBuildException("A Server ID was not provided to the Client Builder.");
+            if (client.listenerAdapters.isEmpty()) {
+                System.out.println("\u001B[33m[WARNING]: No Listener Adapters have been added.\u001B[0m");
+            }
         }
 
         /**
