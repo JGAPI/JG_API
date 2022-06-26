be375129b8eecfe35020bdb2b1511582b6598937
Jared <TheWolfBadger@gmail.com>
2022-06-26 16:00:47 -0400

    + Current code changes for Nathaniel to grab

--- src/main/java/dev/jgapi/jg_api/entities/server/ServerModel.java
+++ src/main/java/dev/jgapi/jg_api/entities/server/ServerModel.java
@@ -1,8 +1,11 @@
 package dev.jgapi.jg_api.entities.server;
 
+import dev.jgapi.jg_api.JG_API;
+import dev.jgapi.jg_api.entities.GuildedObject;
+
 import java.time.Instant;
 
-public class ServerModel {
+public class ServerModel extends GuildedObject {
     private String id;
     private String ownerId;
     private String type;
@@ -15,4 +18,68 @@ public class ServerModel {
     private boolean isVerified;
     private String defaultChannelId;
     private Instant createdAt;
+
+    public ServerModel(JG_API jg_api, String id, String ownerId, String type, String name, String url, String about, String avatar, String banner, String timezone, boolean isVerified, String defaultChannelId, Instant createdAt) {
+        super(jg_api);
+        this.id = id;
+        this.ownerId = ownerId;
+        this.type = type;
+        this.name = name;
+        this.url = url;
+        this.about = about;
+        this.avatar = avatar;
+        this.banner = banner;
+        this.timezone = timezone;
+        this.isVerified = isVerified;
+        this.defaultChannelId = defaultChannelId;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getOwnerId() {
+        return this.ownerId;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getAbout() {
+        return this.about;
+    }
+
+    public String getAvatar() {
+        return this.avatar;
+    }
+
+    public String getBanner() {
+        return this.banner;
+    }
+
+    public String getTimezone() {
+        return this.timezone;
+    }
+
+    public boolean isVerified() {
+        return this.isVerified;
+    }
+
+    public String getDefaultChannelId() {
+        return this.defaultChannelId;
+    }
+
+    public Instant getCreatedAt() {
+        return this.createdAt;
+    }
 }
--- src/main/java/dev/jgapi/jg_api/rest/RestQueue.java
+++ src/main/java/dev/jgapi/jg_api/rest/RestQueue.java
@@ -1,9 +1,26 @@
 package dev.jgapi.jg_api.rest;
 
 
+import dev.jgapi.jg_api.JG_API;
+import dev.jgapi.jg_api.entities.GuildedObject;
+import dev.jgapi.jg_api.entities.calendars.CalendarEvent;
+import dev.jgapi.jg_api.entities.channels.Mentions;
+import dev.jgapi.jg_api.entities.channels.ServerChannel;
+import dev.jgapi.jg_api.entities.chat.ChatEmbed;
+import dev.jgapi.jg_api.entities.chat.ChatMessage;
+import dev.jgapi.jg_api.entities.docs.Doc;
+import dev.jgapi.jg_api.entities.forums.ForumTopic;
 import dev.jgapi.jg_api.entities.http.HttpResponseEntity;
+import dev.jgapi.jg_api.entities.listitems.ListItem;
+import dev.jgapi.jg_api.entities.memberbans.ServerMemberBan;
+import dev.jgapi.jg_api.entities.members.ServerMember;
+import dev.jgapi.jg_api.entities.server.ServerModel;
+import dev.jgapi.jg_api.entities.webhooks.Webhook;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,15 +41,15 @@ public class RestQueue {
     public void removeBySequenceNumber(long seqNumber) {
         this.RestQueue.removeIf(item -> item.getSequenceNumber() == seqNumber);
     }
-    public void processQueue() throws IOException {
+
+    public void processRestAction(RestAction<?> action) throws IOException {
         if (this.isQueueEmpty()) return;
         // It's not empty, we want to process it
-        RestAction<?> action = this.RestQueue.get(0);
         HttpResponseEntity resp = action.getRequest().execute(TIMEOUT);
         switch (resp.getResponseCode()) {
             case 200:
             case 201:
-                action.getOnSuccess().accept(processAction(resp.getResponse(), action.getRequest().getRoute().getReturnType()));
+                action.getOnSuccess().accept(processAction(action.get_JGAPI(), resp.getResponse(), action.getRequest().getRoute().getReturnType()));
                 break;
             case 204:
                 // Error
@@ -44,41 +61,119 @@ public class RestQueue {
         }
     }
 
-    public <T> T processAction(String jsonResponse, Routing.ReturnType returnType) {
+    public void processQueue() throws IOException {
+        if (this.isQueueEmpty()) return;
+        // It's not empty, we want to process it
+        RestAction<?> action = this.RestQueue.get(0);
+        this.processRestAction(action);
+    }
+
+    public <T> T processAction(JG_API jg_api, String jsonResponse, Routing.ReturnType returnType) {
         // Example:
         // new RestAction<Webhook>(this.jg_api.getNextSeqNumber(), request, this.jg_api).queue(webhook -> { webhook.getServerId(); });
-        return switch (returnType) {
-            case NONE -> (T) Boolean.TRUE;
-            case ServerModel -> null;
-            case ServerChannel -> null;
-            case ChatMessage -> null;
-            case ChatMessage_Arr -> null;
-            case Nickname -> (T) "";
-            case ServerMember -> null;
-            case ServerMemberSummary_Arr -> null;
-            case ServerMemberBan -> null;
-            case ServerMemberBan_Arr -> null;
-            case ForumTopic -> null;
-            case ListItem -> null;
-            case ListItemSummary_Arr -> null;
-            case ListItem_Update_Obj -> null;
-            case Doc -> null;
-            case Doc_Arr -> null;
-            case XP_Member_Total -> null;
-            case XP_Role_Total -> null;
-            case Social_Links_Obj -> null;
-            case MemberRoles -> null;
-            case Webhook -> null;
-            case Webhook_Arr -> null;
-            case CalendarEvent -> null;
-            case CalendarEvent_Arr -> null;
-        };
+        JSONObject json = new JSONObject(jsonResponse);
+        String serverId = json.optString("serverId", null);
+        switch (returnType) {
+            case NONE -> {
+                return (T) Boolean.TRUE;
+            }
+            case ServerModel -> {
+                return null;
+            }
+            case ServerChannel -> {
+                JSONObject channel = json.getJSONObject("channel");
+                return (T) new ServerChannel(jg_api, channel.getString("id"), channel.getString("type"), channel.getString("name"), channel.optString("topic", ""), Instant.parse(channel.getString("createdAt")), channel.getString("createdBy"), Instant.parse(channel.optString("updatedAt", "")), channel.getString("serverId"), channel.optString("parentId", ""), channel.optString("parentId", ""), channel.getString("groupId"), channel.optBoolean("isPublic", false), channel.optString("archivedBy", null), Instant.parse(channel.optString("archivedAt", "")));
+            }
+            case ChatMessage -> {
+                JSONObject chatMessage = json.getJSONObject("message");
+                JSONArray embedsJSON = chatMessage.optJSONArray("embeds");
+                String[] replyMessageIds = null;
+                ChatEmbed[] embeds = null;
+                Mentions mentions = null;
+                return (T) new ChatMessage(jg_api, chatMessage.getString("id"), chatMessage.getString("type"), chatMessage.optString("serverId", ""), chatMessage.getString("channelId"), chatMessage.getString("content"), embeds, replyMessageIds, chatMessage.optBoolean("isPrivate", false), chatMessage.optBoolean("isSilent", false), mentions, Instant.parse(chatMessage.getString("createdAt")), chatMessage.getString("createdBy"), chatMessage.optString("createdByWebhookId", null), Instant.parse(chatMessage.optString("updatedAt", null)));
+            }
+            case ChatMessage_Arr -> {
+                // TODO
+                return null;
+            }
+            case Nickname -> {
+                // TODO
+                return null;
+            }
+            case ServerMember -> {
+                return null;
+            }
+            case ServerMemberSummary_Arr -> {
+                // TODO
+                return null;
+            }
+            case ServerMemberBan -> {
+                return null;
+            }
+            case ServerMemberBan_Arr -> {
+                // TODO
+                return null;
+            }
+            case ForumTopic -> {
+                return null;
+            }
+            case ListItem -> {
+                return null;
+            }
+            case ListItemSummary_Arr -> {
+                // TODO
+                return null;
+            }
+            case ListItem_Update_Obj -> {
+                // TODO
+                return null;
+            }
+            case Doc -> {
+                return null;
+            }
+            case Doc_Arr -> {
+                // TODO
+                return null;
+            }
+            case XP_Member_Total -> {
+                // TODO
+                return null;
+            }
+            case XP_Role_Total -> {
+                // TODO
+                return null;
+            }
+            case Social_Links_Obj -> {
+                // TODO
+                return null;
+            }
+            case MemberRoles -> {
+                // TODO
+                return null;
+            }
+            case Webhook -> {
+                return null;
+            }
+            case Webhook_Arr -> {
+                // TODO
+                return null;
+            }
+            case CalendarEvent -> {
+                return null;
+            }
+            case CalendarEvent_Arr -> {
+                // TODO
+                return null;
+            }
+        }
+        return null;
     }
 
     public List<RestAction<?>> getQueuedRestActions() {
         return this.RestQueue;
     }
-    public boolean containsSequenceNumber(long seqNumber) {
+
+    public boolean hasSequenceNumber(long seqNumber) {
         long count = this.RestQueue.stream().filter(item -> item.getSequenceNumber() == seqNumber).count();
         return (count >= 1);
     }
