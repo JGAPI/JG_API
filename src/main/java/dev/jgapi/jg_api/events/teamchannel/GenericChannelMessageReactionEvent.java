dc43b122e07264a0faaa30e96f7fbf8248166459
Jared <TheWolfBadger@gmail.com>
2022-07-06 22:04:39 -0400

    + Minor fix and code cleanup

--- src/main/java/dev/jgapi/jg_api/events/teamchannel/GenericChannelMessageReactionEvent.java
+++ src/main/java/dev/jgapi/jg_api/events/teamchannel/GenericChannelMessageReactionEvent.java
@@ -4,8 +4,8 @@ import dev.jgapi.jg_api.JG_API;
 import dev.jgapi.jg_api.entities.channels.ChannelReaction;
 import dev.jgapi.jg_api.events.Event;
 
-public class GenericChannelMessageReactionEvent extends Event {
-    private ChannelReaction reaction;
+public abstract class GenericChannelMessageReactionEvent extends Event {
+    ChannelReaction reaction;
     public GenericChannelMessageReactionEvent(JG_API jg_api, String serverId, ChannelReaction reaction) {
         super(jg_api, serverId);
         this.reaction = reaction;
